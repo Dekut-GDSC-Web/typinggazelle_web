12365bcf02b8b3536d0c1a6e60b8bf5e10b0fbc6
Gilbertkitetu <gilbertkitetu2@gmail.com>
2022-03-08 21:35:57 +0300

    keyboard

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import { Button, Navbar, Container } from 'react-bootstrap';
+
 
 //+++ import components +++
 import Virtualkeyboard from "./components/Virtualkeyboard";
@@ -13,18 +15,27 @@ function App() {
   return (
     <div className="App">
       <Navbar>
-  <Container>
-    <Navbar.Brand href="#home">TypingGazelle</Navbar.Brand>
-    <Navbar.Toggle />
-    <Navbar.Collapse className="justify-content-end">
-      <Navbar.Text>
-        <a href="#login">Login</a>
-      </Navbar.Text>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-      {/* <Login /> */}
-      <Virtualkeyboard />
+        <Container>
+          <Navbar.Brand href="/">TypingGazelle</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <a href="/login">Login</a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+
+      <BrowserRouter>
+        <Routes>
+         <Route path = "/" element={<Virtualkeyboard /> } />
+          <Route path="login/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+
+
+      
 
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+
+
+import { Button, Container, Card, Col, Row, Form, CardImg } from 'react-bootstrap';
 
 function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <Row>
+        <Col>
+        <img src="https://images.pexels.com/photos/3829230/pexels-photo-3829230.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/>
+        </Col>
+        <Col>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+          <h6>Don't have an account? <a href="/signup">Sign up</a></h6>
+        </Col>
+      </Row>
+    </div>
   )
 }
 
