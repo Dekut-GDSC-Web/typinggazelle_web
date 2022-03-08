0a6aced07578ff66c3c2b75695e2ead3768e1d27
Gilbertkitetu <gilbertkitetu2@gmail.com>
2022-03-08 21:00:30 +0300

    keyboard

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Button, Navbar, Container } from 'react-bootstrap';
 
 //+++ import components +++
 import Virtualkeyboard from "./components/Virtualkeyboard";
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar>
+  <Container>
+    <Navbar.Brand href="#home">TypingGazelle</Navbar.Brand>
+    <Navbar.Toggle />
+    <Navbar.Collapse className="justify-content-end">
+      <Navbar.Text>
+        <a href="#login">Login</a>
+      </Navbar.Text>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+      {/* <Login /> */}
       <Virtualkeyboard />
-      
+
     </div>
   );
 }
--- src/components/Virtualkeyboard.js
+++ src/components/Virtualkeyboard.js
@@ -1,33 +1,77 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Keyboard from 'react-simple-keyboard';
 import 'react-simple-keyboard/build/css/index.css';
 
 
-
+import { Button, Container, Card, Col, Row } from 'react-bootstrap';
 
 function Virtualkeyboard() {
 
-    //hooks
-    const [state, setState] = useState("");
 
 
-    //functions
-    var onChange = (input) => {
-      console.log("Input changed", input);
-    }
-  
-    var onKeyPress = (button) => {
-      console.log("Button pressed", button);
-    }
-  
+  const [input, setInput] = useState("");
+  const [layout, setLayout] = useState("default");
+  const keyboard = useRef();
+
+  const onChange = input => {
+    setInput(input);
+    console.log("Input changed", input);
+  };
+
+  const handleShift = () => {
+    const newLayoutName = layout === "default" ? "shift" : "default";
+    setLayout(newLayoutName);
+  };
+
+  const onKeyPress = button => {
+    console.log("Button pressed", button);
+
+    /**
+     * If you want to handle the shift and caps lock buttons
+     */
+    if (button === "{shift}" || button === "{lock}") handleShift();
+  };
+
+  const onChangeInput = event => {
+    const input = event.target.value;
+    setInput(input);
+    keyboard.current.setInput(input);
+  };
+
+
 
 
   return (
     <div>
-     <Keyboard
-        onChange={onChange()}
-        onKeyPress={onKeyPress()}
+      <Row>
+        <Col></Col>
+      <Col sm={8} xs={12} md={8}>
+      <Card>
+        <Card.Body>
+          <input className='inputcard'
+            value={input}
+            placeholder={"Tap on the virtual keyboard to start"}
+            onChange={onChangeInput}
+          />
+        </Card.Body>
+      </Card>
+      </Col>
+      <Col></Col>
+      </Row>
+
+      <Row>
+        <Col></Col>
+      <Col sm={8} xs={12} md={8}>
+      <Keyboard
+        keyboardRef={r => (keyboard.current = r)}
+        layoutName={layout}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
       />
+       </Col>
+      <Col></Col>
+      </Row>
+
     </div>
   )
 }
